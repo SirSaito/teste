ef2fc16126fe77bbdf3abee69f307918e6fecc7d
SirSaito <89697576+SirSaito@users.noreply.github.com>
2024-11-18 09:22:59 -0300

    Add files via upload

---
+++ teste.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<assert.h>
+
+int registro_teste(int r){
+    assert(r==r);
+    printf("\t\tprimeiro teste correto\n");
+    assert(r==99929);
+    printf("\t\tsegundo teste correto\n");
+    return(r);
+}
+
+int main(){
+    int r;
+    printf("\t\tSeu registro e 99929\n");
+    printf("\t\tEntre seu registro na locadora:\n");
+    scanf("%d", &r);
+    registro_teste(r);
+    printf("\t\tVerificacao de registros");
+    return 0;
+}
